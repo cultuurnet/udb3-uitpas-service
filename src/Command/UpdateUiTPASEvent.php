5b73e1a45d0a0d1b09e7de1ee46eae0d5f9dd65c
Bert Ramakers <bertramakers@me.com>
2016-10-28 14:21:33 +0200

    III-1491: Command for updating existing uitpas events.

---
+++ src/Command/UpdateUiTPASEvent.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CultuurNet\UDB3\UiTPASService\Command;
+
+use CultuurNet\UDB3\PriceInfo\PriceInfo;
+
+class UpdateUiTPASEvent
+{
+    /**
+     * @var string
+     */
+    private $eventId;
+
+    /**
+     * @var string
+     */
+    private $organizerId;
+
+    /**
+     * @var PriceInfo
+     */
+    private $priceInfo;
+
+    /**
+     * @param $eventId
+     * @param $organizerId
+     * @param PriceInfo $priceInfo
+     */
+    public function __construct(
+        $eventId,
+        $organizerId,
+        PriceInfo $priceInfo
+    ) {
+        $this->eventId = $eventId;
+        $this->organizerId = $organizerId;
+        $this->priceInfo = $priceInfo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventId()
+    {
+        return $this->eventId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrganizerId()
+    {
+        return $this->organizerId;
+    }
+
+    /**
+     * @return PriceInfo
+     */
+    public function getPriceInfo()
+    {
+        return $this->priceInfo;
+    }
+}
