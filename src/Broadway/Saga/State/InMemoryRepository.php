09009e510777dacb6cb05067db9e517168e9d3e2
Bert Ramakers <bertramakers@me.com>
2017-01-20 17:01:29 +0100

    III-1804: Updated UiTPASEventSagaTest to use forked broadway saga classes to have a more realistic unit test.

---
+++ src/Broadway/Saga/State/InMemoryRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CultuurNet\UDB3\UiTPASService\Broadway\Saga\State;
+
+use Broadway\Saga\State;
+use Broadway\Saga\State\Criteria;
+
+class InMemoryRepository implements RepositoryInterface
+{
+    private $states = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findBy(Criteria $criteria, $sagaId)
+    {
+        if (!isset($this->states[$sagaId])) {
+            $states = [];
+        } else {
+            $states = $this->states[$sagaId];
+        }
+
+        foreach ($criteria->getComparisons() as $key => $value) {
+            $states = array_filter($states, function ($elem) use ($key, $value) {
+                $stateValue = $elem->get($key);
+
+                return is_array($stateValue) ? in_array($value, $stateValue) : $value === $stateValue;
+            });
+        }
+
+        foreach ($states as $state) {
+            yield $state;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function save(State $state, $sagaId)
+    {
+        if ($state->isDone()) {
+            unset($this->states[$sagaId][$state->getId()]);
+        } else {
+            $this->states[$sagaId][$state->getId()] = $state;
+        }
+    }
+}
