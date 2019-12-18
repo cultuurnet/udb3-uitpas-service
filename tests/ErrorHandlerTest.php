bc18554a7b2351f0a2c5d14d365e09a04de768d8
Bert Ramakers <bert@madewithlove.be>
2019-12-18 16:15:29 +0100

    III-3173 Fix ErrorHandler test

--- tests/ErrorHandlerTest.php
+++ tests/ErrorHandlerTest.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UDB3\UiTPASService;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class ErrorHandlerTest extends TestCase
@@ -11,12 +12,16 @@ class ErrorHandlerTest extends TestCase
      */
     public function it_creates_api_problem_from_exception()
     {
-        $exception = new \Exception('Exception message', 500);
+        $exception = new Exception('Exception message', 500);
 
         $errorHandler = new ErrorHandler();
-        $apiProblem = $errorHandler->__invoke($exception);
-        $this->assertEquals('Exception message', $apiProblem->getTitle());
-        $this->assertEquals(500, $apiProblem->getStatus());
+        $apiProblemResponse = $errorHandler->__invoke($exception);
+
+        $this->assertEquals(
+            '{"title":"Exception message","type":"about:blank","status":500}',
+            $apiProblemResponse->getContent()
+        );
+        $this->assertEquals(500, $apiProblemResponse->getStatusCode());
     }
 
     /**
@@ -24,12 +29,16 @@ class ErrorHandlerTest extends TestCase
      */
     public function it_creates_api_problem_with_400_status_if_exception_has_no_status_code()
     {
-        $exception = new \Exception('Exception message');
+        $exception = new Exception('Exception message');
 
         $errorHandler = new ErrorHandler();
-        $apiProblem = $errorHandler->__invoke($exception);
-        $this->assertEquals('Exception message', $apiProblem->getTitle());
-        $this->assertEquals(400, $apiProblem->getStatus());
+        $apiProblemResponse = $errorHandler->__invoke($exception);
+
+        $this->assertEquals(
+            '{"title":"Exception message","type":"about:blank","status":400}',
+            $apiProblemResponse->getContent()
+        );
+        $this->assertEquals(400, $apiProblemResponse->getStatusCode());
     }
 
     /**
@@ -39,10 +48,14 @@ class ErrorHandlerTest extends TestCase
     {
         // the resulting ApiProblem title should be stripped
         // of everything after URL CALLED
-        $exception = new \Exception('Exception message URL CALLED: https://acc.uitid.be/uitid/rest/uitpas/cultureevent/de343e38-d656-4928-96bc-55578e0d94ec/cardsystems POST DATA: cardSystemId=3');
+        $exception = new Exception('Exception message URL CALLED: https://acc.uitid.be/uitid/rest/uitpas/cultureevent/de343e38-d656-4928-96bc-55578e0d94ec/cardsystems POST DATA: cardSystemId=3');
 
         $errorHandler = new ErrorHandler();
-        $apiProblem = $errorHandler->__invoke($exception);
-        $this->assertEquals('Exception message ', $apiProblem->getTitle());
+        $apiProblemResponse = $errorHandler->__invoke($exception);
+
+        $this->assertEquals(
+            '{"title":"Exception message ","type":"about:blank","status":400}',
+            $apiProblemResponse->getContent()
+        );
     }
 }
