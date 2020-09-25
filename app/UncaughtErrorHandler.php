59b954a2ea154708aaf3a5088f3b48f702029957
Luc Wollants <lucwollants@gmail.com>
2020-09-25 12:49:17 +0200

    III-3386 Add new UncaughtErrorHandler

---
+++ app/UncaughtErrorHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\UiTPASService;
+
+use Sentry\State\HubInterface;
+use Throwable;
+
+class UncaughtErrorHandler
+{
+    /** @var HubInterface */
+    private $sentryHub;
+
+    public function __construct(HubInterface $sentryHub)
+    {
+        $this->sentryHub = $sentryHub;
+    }
+
+    public function handle(Throwable $throwable): void
+    {
+        if ($throwable->getCode() !== 404) {
+            $this->sentryHub->captureException($throwable);
+        }
+    }
+}
