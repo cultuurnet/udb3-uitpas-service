3d1d26e63d487cc89c13dab40271b88c001afd7f
Luc Wollants <lucwollants@gmail.com>
2020-09-25 12:51:44 +0200

    III-3386 Bootstrap Sentry

--- web/index.php
+++ web/index.php
@@ -9,6 +9,7 @@ use CultuurNet\UDB3\UiTPASService\ApiGuardServiceProvider;
 use CultuurNet\UDB3\UiTPASService\Controller\EventControllerProvider;
 use CultuurNet\UDB3\UiTPASService\Controller\OrganizerControllerProvider;
 use CultuurNet\UDB3\UiTPASService\ErrorHandlerProvider;
+use CultuurNet\UDB3\UiTPASService\SentryServiceProvider;
 use Silex\Application;
 use Silex\Provider\SecurityServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
@@ -27,6 +28,11 @@ $app->register(new ServiceControllerServiceProvider());
  * Check api keys for requests.
  */
 $app->register(new ApiGuardServiceProvider());
+
+/**
+ * Setup Sentry to capture uncaught exceptions.
+ */
+$app->register(new SentryServiceProvider());
 
 /**
  * Handle errors by returning an API problem response.
