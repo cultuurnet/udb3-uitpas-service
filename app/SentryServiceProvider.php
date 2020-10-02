c0ab4afa976f1d84a940bc4367b8962409f59ae4
Luc Wollants <lucwollants@gmail.com>
2020-10-02 11:03:18 +0200

    III-3386 Fix Sentry SDK hub initialisation.

--- app/SentryServiceProvider.php
+++ app/SentryServiceProvider.php
@@ -4,24 +4,24 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\UiTPASService;
 
-use Sentry\ClientBuilder;
-use Sentry\State\Hub;
+use Sentry\SentrySdk;
 use Sentry\State\HubInterface;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use function Sentry\init;
 
 class SentryServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app): void
     {
         $app[HubInterface::class] = $app->share(
-            function ($app) {
-                return new Hub(
-                    ClientBuilder::create([
-                        'dsn' => $app['config']['sentry']['dsn'],
-                        'environment' => $app['config']['sentry']['environment'],
-                    ])->getClient()
-                );
+            function (Application $app) {
+                init([
+                    'dsn' => $app['config']['sentry']['dsn'],
+                    'environment' => $app['config']['sentry']['environment'],
+                ]);
+
+                return SentrySdk::getCurrentHub();
             }
         );
     }
